ac3e605e913411178f70fbc717cf06ab71d7961b
adrianno3259 <adriannosampaio@hotmail.com>
2017-12-13 03:55:31 -0200

    Start rayInsideBox

--- tracerHLS.c
+++ tracerHLS.c
@@ -1,13 +1,19 @@
 #include "tracerHLS.h"
+#include <stdio.h>
 
 #define TRIANGLE_ATTRIBUTE_NUMBER 13
 #define LIGHT_ATTRIBUTE_NUMBER 7
 #define MATERIAL_ATTRIBUTE_NUMBER 4
 #define RAY_ATTRIBUTE_NUMBER 6
+// data
+float *m_data, *l_data, *r_data, *t_data;
+int num_m, num_l, num_r, num_v, num_t, *v_data;
 
-float *m_data, *l_data, *r_data, *v_data, *t_data;
-int num_m, num_l, num_r, num_v, num_t;
+//bounding box da grid
+float gridMin_x, gridMin_y, gridMin_z, gridMax_x, gridMax_y, gridMax_z;
+int nx, ny, nz;
 
+// interseção final
 int hit, entering;
 double t;
 float hitPoint[3];
@@ -28,12 +34,8 @@ float colorOut[1024*1024*3];
 int nColor;
 
 
-float max3(float a, float b, float c){
-    return (a>b ? (a>c? a : c) : (b>c? b : c));
-}
-
-
 void setlocalHitPoint(int rayID, float t){
+    //printf("t = %f\n\n\n",t);
     localHitPoint[0] = t*r_data[rayID+3] + r_data[rayID];
     localHitPoint[1] = t*r_data[rayID+4] + r_data[rayID+1];
     localHitPoint[2] = t*r_data[rayID+5] + r_data[rayID+2];
@@ -102,11 +104,12 @@ void triangleIntersect(int tID, int rayID){
                 float e3 = a*p - b*r + d*s;
                 float tmin = e3 * inv_denom;
                 if(tmin > 0.00001){
+                    printf("---- %f\n", tri_normaly(tID));
                     localT = tmin;
-                    setlocalHitPoint(rayID, t);
-                    normal[0] = tri_normalx(tID),
-                    normal[1] = tri_normaly(tID),
-                    normal[2] = tri_normalz(tID);
+                    setlocalHitPoint(rayID, localT);
+                    localNormal[0] = tri_normalx(tID),
+                    localNormal[1] = tri_normaly(tID),
+                    localNormal[2] = tri_normalz(tID);
                     localHit = 1;
                     localEntering = 1;
                 }
@@ -115,6 +118,7 @@ void triangleIntersect(int tID, int rayID){
     }
 }
 
+/*
 void colorClamp()
 {
     float maxVal = (r, std::max(g, b));
@@ -176,21 +180,30 @@ void rayHitObjects(int rayID){
         }
     }
 }
-/*
+*/
+
+#define pInt(A) printf("%s = %d\n", #A, A); fflush(stdout);
+#define pFloat(A) printf("%s = %f\n", #A, A); fflush(stdout);
+
 void rayHitObjectsVoxel(int rayID, int voxelID){
 
     double tmin = 1000000.0;
     int i, id = 0;
-    localHit = false;
+    localHit = 0;
+    // ultimo triangulo do voxel
+    pInt(voxelID)
+    int end = (voxelID > 0) ? (v_data[voxelID] - v_data[voxelID-1]): v_data[voxelID];
+    pInt(end)
 
-    for(i = 0; i < v_data[voxelID]; i++){
-        int triangleID = (voxelID > 0) ? v_data[voxelID-1] + 1 + i : i;
-
+    for(i = 0; i < end; i++){
+        int triangleID = (voxelID > 0) ? v_data[voxelID-1] + i : i;
+        //printf("-------------t---------\n"); fflush(stdout);
+        pInt(triangleID);
         triangleIntersect(triangleID, rayID);
-
+        //printf("-------------t---------\n"); fflush(stdout);
         if(localHit && localT < tmin){
-            setlocalHitPoint(rayID, tmin);
-            t = localT
+            //setlocalHitPoint(rayID, tmin);
+            t = localT;
             tmin = localT;
 
             hitPoint[0] = localHitPoint[0],
@@ -201,15 +214,19 @@ void rayHitObjectsVoxel(int rayID, int voxelID){
             normal[1] = localNormal[1],
             normal[2] = localNormal[2];
 
-            hit = true;
-            entering = true;
+            hit = 1;
+            entering = 1;
             id = i;
         }
     }
 }
 
-double max3(double a, double b, double c){
+float max3(float a, float b, float c){
     return (a > b ? (a > c ? a : c) : (b > c ? b : c));
+}
+
+float min3(float a, float b, float c){
+    return (a < b ? (a < c ? a : c) : (b < c ? b : c));
 }
 
 int boundingBoxHit(float minx, float miny, float minz,
@@ -219,7 +236,7 @@ int boundingBoxHit(float minx, float miny, float minz,
            dx = r_data[rayID+3], dy = r_data[rayID+4], dz = r_data[rayID+5];
 
     double tx_min, ty_min, tz_min;
-    double tx_max, ty_max, tz_min;
+    double tx_max, ty_max, tz_max;
 
     double a = 1.0/dx;
     if(a >= 0){
@@ -253,16 +270,35 @@ int boundingBoxHit(float minx, float miny, float minz,
     t0 = max3(tx_min, ty_min, tz_min);
     t1 = min3(tx_max, ty_max, tz_max);
 
-    return (t0 < t1 && t1 > K_EPSILON);
+    return (t0 < t1 && t1 > 0.00001);
+}
+
+int rayInsideBox(int rayID)
+{
+    int inside = 1;
+
 }
 
 void hitGrid(int rayID){
 
+    int ix, iy, iz;
 
+    float tx_min = gridMin_x, ty_min = gridMin_y, tz_min = gridMin_z;
+    float tx_max = gridMax_x, ty_max = gridMax_y, tz_max = gridMax_z;
+
+    float   dtx = (tx_max - tx_min)/nx,
+            dty = (ty_max - ty_min)/ny,
+            dtz = (tz_max - tz_min)/nz;
+
+    float tx_next, ty_next, tz_next;
+    int ix_step, iy_step, iz_step;
+    int ix_stop, iy_stop, iz_stop;
+
+    tx_next = tx_min + ()
 
 
 }
-
+/*
 // Salva no array global de cores o resultado do tracing
 void rayTraceSimple(int rayID){
     double tmin = 1e15; int i;
@@ -284,4 +320,65 @@ void tracer(int nVoxels, float* voxel_data, int nTriangles, float* triangle_data
         rayTraceSimple(i);
     }
 
+}
+
+*/
+
+
+int main(){
+
+    float triangleData[TRIANGLE_ATTRIBUTE_NUMBER*2];
+    float rayData[RAY_ATTRIBUTE_NUMBER];
+    int voxelData[2];
+
+    voxelData[0] = 0;
+    voxelData[1] = 2;
+    /*
+
+    Ray testRay = rayNew(vecNew(0, -1, 0), vecNew(0, 1, 0));
+    Triangle testTriangle = triangleNew(vecNew(1,2,-1), vecNew(-1, 2, -1), vecNew(0, 2, 1));
+    Intersect inter = triangleIntersect(testTriangle, testRay);
+    PV(inter.hitPoint); printf("%s %f", "inter.t", inter.t);
+    */
+
+    triangleData[0] = 1, triangleData[1] = 2, triangleData[2] = -1; // point 1
+    triangleData[3] = -1, triangleData[4] = 2, triangleData[5] = -1; // point 2
+    triangleData[6] = 0, triangleData[7] = 2, triangleData[8] = 1; // point 3
+    triangleData[9] = 0, triangleData[10] = -1, triangleData[11] = 0; // normal
+    triangleData[13] = 0;
+
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+0] = 1,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+1] = 3,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+2] = -1; // point 1
+
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+3] = -1,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+4] = 3,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+5] = -1; // point 2
+
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+6] = 0,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+7] = 3,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+8] = 1; // point 3
+
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+9] = 0,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+10] = -1,
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+11] = 0; // normal
+    triangleData[TRIANGLE_ATTRIBUTE_NUMBER+13] = 0;
+
+    rayData[0] = 0, rayData[1] = -1, rayData[2] = 0;
+    rayData[3] = 0, rayData[4] = 1, rayData[5] = 0;
+
+    r_data = rayData, t_data = triangleData, v_data = voxelData;
+    num_r = 1, num_t = 2, num_v = 2;
+
+    //triangleIntersect(0, 0);
+    rayHitObjectsVoxel(0, 1);
+
+    pFloat(t) pFloat(hitPoint[0]) pFloat(hitPoint[1]) pFloat(hitPoint[2])
+
+    printf("t = %f\nhitPoint = %f, %f, %f\n", localT, localHitPoint[0], localHitPoint[1], localHitPoint[2]);
+    printf("Normal = %f, %f, %f\n", localNormal[0], localNormal[1], localNormal[2]);
+
+    //pInt(boundingBoxHit(-1, 0, -1, 1, 1, 1, 0))
+
+    return 0;
 }
